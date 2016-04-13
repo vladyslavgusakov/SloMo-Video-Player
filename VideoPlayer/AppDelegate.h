c0123a603886bcd59300ff609d40fff4e5bc7d6e
Vladyslav Gusakov <vladyslavgusakov@gmail.com>
2016-04-13 10:37:21 -0400

    Initial Commit

---
+++ VideoPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VideoPlayer
+//
+//  Created by Vladyslav Gusakov on 4/13/16.
+//  Copyright © 2016 Vladyslav Gusakov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VideoPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VideoPlayer
+//
+//  Created by Vladyslav Gusakov on 4/13/16.
+//  Copyright © 2016 Vladyslav Gusakov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
